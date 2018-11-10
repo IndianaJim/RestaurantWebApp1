66580a2da51e7d8c83f262b4cd40d7215cfdc2d0
indianajim <jimrunkel@gmail.com>
2018-11-10 14:17:16 -0500

    nav menu start

--- assets/js/hyper/components/Header.js
+++ assets/js/hyper/components/Header.js
@@ -1,7 +1,18 @@
 import { h, app } from 'hyperapp';
 
 export default function Header({ state, actions }) {
-  return <header>Header component</header>;
+  return (
+    <header>
+      <div class="logo">Logo</div>
+      <nav>
+        <a href="#">Menu</a>
+        <a href="#">Party Platters</a>
+        <a href="#">Locations</a>
+        <a href="#">Rewards</a>
+        <a href="#">Reservations</a>
+      </nav>
+    </header>
+  );
 }
 // <Header state={state} actions={actions}/>
 // <Button state={state} actions={actions}/>
